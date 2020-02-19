8e9febed682c3bb916a4938d5efa6175f8ee0bad
Bhager01 <61237715+Bhager01@users.noreply.github.com>
2020-02-19 11:31:25 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,15 +1,190 @@
 const Discord = require('discord.js');
+const {google} = require('googleapis');
 const client = new Discord.Client();
+const prefix = "!"
 
-client.on('ready', () => {
-    console.log('I am ready!');
-});
+client.once('ready', () => {
+    console.log('Ready')
+}) 
+
+function FlairUpdate(){
+    const guild = client.guilds.get("505515654833504266");
+
+    content = {"installed":{"client_id":"842290271074-u9kfivj3l2i5deugh3ppit9mo6i8oltr.apps.googleusercontent.com","project_id":"mhanndalorian-1581969700452","auth_uri":"https://accounts.google.com/o/oauth2/auth","token_uri":"https://oauth2.googleapis.com/token","auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs","client_secret":"ZPufJMDMo8OuJ-JxOk6X3OXw","redirect_uris":["urn:ietf:wg:oauth:2.0:oob","http://localhost"]}}
+    authorize(content, listMajors);
+      
+      function authorize(credentials, callback) {
+        const {client_secret, client_id, redirect_uris} = credentials.installed;
+        const oAuth2Client = new google.auth.OAuth2(
+            client_id, client_secret, redirect_uris[0]);
+      
+          token = {"access_token":"ya29.Il-9BygCO5hRduHR-tUsBx32geTiZxDF4QUjh17uDovL_OQYrsW-q53oknT-PYfQbG6qMAvDeV4myI3_uKIYIQLMsFPIuRV0UTR4g31GFJpdOuv-uQwqm1I-g4ttX0CgDg","refresh_token":"1//0dhkLg8Xv7BDXCgYIARAAGA0SNwF-L9IrG-vrIlgGQGioOCDU2gilJp7ZHgDWgiiugPjQWGw091GlSXJx4fTJJ5-6XIZYu5p_7Ds","scope":"https://www.googleapis.com/auth/spreadsheets.readonly","token_type":"Bearer","expiry_date":1581974001623}
+          oAuth2Client.setCredentials(token);
+          callback(oAuth2Client);
+      }
+      
+      function listMajors(auth) {
+        const sheets = google.sheets({version: 'v4', auth});
+        sheets.spreadsheets.values.get({
+          spreadsheetId: '1p5nViz3_kCnurF9sHZE1PGsu22RXxh-qf_7JkonbipQ',
+          range: 'Guild Members & Data!E119:F',
+        }, (err, res) => {
+          if (err) return console.log('The API returned an error: ' + err);
+          const rows = res.data.values;
+          if (rows.length) {
+
+          var GuildMember;
+          var User;
+          var discordID;
+          var count = 0;
+
+            rows.map((row) => {
+                (async () => {
+                      discordID = row[1].replace("<","").replace(">","").replace("@","");
+                      if(discordID != 378053516067078149){
+                        User = await client.fetchUser(discordID)
+                        GuildMember = await guild.fetchMember(User);
+                        AddFlair(GuildMember,row[0]);
+                        count = count + 1
+                        console.log(count)
+                      }
+                })()
+            });
+          } else {
+            console.log('No data found.');
+          }
+        });
+    }
+}
+
+var CronJob = require('cron').CronJob;
+var job = new CronJob('30/60 19-21 * * *', function() {
+    FlairUpdate()
+    
+}, null, true, 'America/New_York');
+job.start();
+
+function AddFlair(passedMember, row){
+    if(row <= 13){
+        passedMember.setNickname((passedMember.displayName).replace('🥉','').replace('🥈','').replace('🥇','').replace('💎',''))
+        console.log(passedMember.displayName)
+    }
+
+    if(row >= 14 && row <= 29){
+        passedMember.setNickname((passedMember.displayName).replace('🥉','').replace('🥈','').replace('🥇','').replace('💎','') + '🥉')
+        console.log(passedMember.displayName)
+     }
+    if(row >= 30 && row <= 59){
+        passedMember.setNickname((passedMember.displayName).replace('🥉','').replace('🥈','').replace('🥇','').replace('💎','') + '🥈')
+        console.log(passedMember.displayName)
+     }
+    if(row >= 60 && row <= 99){
+        passedMember.setNickname((passedMember.displayName).replace('🥉','').replace('🥈','').replace('🥇','').replace('💎','') + '🥇')
+        console.log(passedMember.displayName)
+     }
+    if(row >= 100){
+        passedMember.setNickname((passedMember.displayName).replace('🥉','').replace('🥈','').replace('🥇','').replace('💎','') + '💎')
+        console.log(passedMember.displayName)
+     }
+
+}
+
+client.on('presenceUpdate', async (oldMember, newMember) => {
+  var content = {"installed":{"client_id":"842290271074-u9kfivj3l2i5deugh3ppit9mo6i8oltr.apps.googleusercontent.com","project_id":"mhanndalorian-1581969700452","auth_uri":"https://accounts.google.com/o/oauth2/auth","token_uri":"https://oauth2.googleapis.com/token","auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs","client_secret":"ZPufJMDMo8OuJ-JxOk6X3OXw","redirect_uris":["urn:ietf:wg:oauth:2.0:oob","http://localhost"]}}
+  authorize(content, listMajors);
+
+    function authorize(credentials, callback) {
+        const {client_secret, client_id, redirect_uris} = credentials.installed;
+        const oAuth2Client = new google.auth.OAuth2(
+        client_id, client_secret, redirect_uris[0]);
+
+        token = {"access_token":"ya29.Il-9BygCO5hRduHR-tUsBx32geTiZxDF4QUjh17uDovL_OQYrsW-q53oknT-PYfQbG6qMAvDeV4myI3_uKIYIQLMsFPIuRV0UTR4g31GFJpdOuv-uQwqm1I-g4ttX0CgDg","refresh_token":"1//0dhkLg8Xv7BDXCgYIARAAGA0SNwF-L9IrG-vrIlgGQGioOCDU2gilJp7ZHgDWgiiugPjQWGw091GlSXJx4fTJJ5-6XIZYu5p_7Ds","scope":"https://www.googleapis.com/auth/spreadsheets.readonly","token_type":"Bearer","expiry_date":1581974001623}
+        oAuth2Client.setCredentials(token);
+        callback(oAuth2Client);
+    }
+
+    function listMajors(auth) {
+        const sheets = google.sheets({version: 'v4', auth});
+        sheets.spreadsheets.values.get({
+            spreadsheetId: '1p5nViz3_kCnurF9sHZE1PGsu22RXxh-qf_7JkonbipQ',
+            range: 'Guild Members & Data!E119:F',
+        }, (err, res) => {
+            if (err) return console.log('The API returned an error: ' + err);
+            const rows = res.data.values;
+            if (rows.length) {
+                rows.map((row) => {
+                    if(String(row[1]).match(/\d+/) == newMember.user.id && String(row[1]).match(/\d+/) != "378053516067078149"){
+                        AddFlair(newMember, row[0]);
+                    }
+                });
+            }else {
+                console.log('No data found.');
+            }
+        });
+    }
+})
 
 client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
-  	}
-});
+      if(message.content == `${prefix}flair` || message.content == `${prefix}Flair`){
+        var content = {"installed":{"client_id":"842290271074-u9kfivj3l2i5deugh3ppit9mo6i8oltr.apps.googleusercontent.com","project_id":"mhanndalorian-1581969700452","auth_uri":"https://accounts.google.com/o/oauth2/auth","token_uri":"https://oauth2.googleapis.com/token","auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs","client_secret":"ZPufJMDMo8OuJ-JxOk6X3OXw","redirect_uris":["urn:ietf:wg:oauth:2.0:oob","http://localhost"]}}
+        authorize(content, listMajors);
+          
+        function authorize(credentials, callback) {
+            const {client_secret, client_id, redirect_uris} = credentials.installed;
+            const oAuth2Client = new google.auth.OAuth2(
+                client_id, client_secret, redirect_uris[0]);
+          
+         token = {"access_token":"ya29.Il-9BygCO5hRduHR-tUsBx32geTiZxDF4QUjh17uDovL_OQYrsW-q53oknT-PYfQbG6qMAvDeV4myI3_uKIYIQLMsFPIuRV0UTR4g31GFJpdOuv-uQwqm1I-g4ttX0CgDg","refresh_token":"1//0dhkLg8Xv7BDXCgYIARAAGA0SNwF-L9IrG-vrIlgGQGioOCDU2gilJp7ZHgDWgiiugPjQWGw091GlSXJx4fTJJ5-6XIZYu5p_7Ds","scope":"https://www.googleapis.com/auth/spreadsheets.readonly","token_type":"Bearer","expiry_date":1581974001623}
+         oAuth2Client.setCredentials(token);
+         callback(oAuth2Client);
+        }
+          
+        function listMajors(auth) {
+            const sheets = google.sheets({version: 'v4', auth});
+            sheets.spreadsheets.values.get({
+                spreadsheetId: '1p5nViz3_kCnurF9sHZE1PGsu22RXxh-qf_7JkonbipQ',
+                range: 'Guild Members & Data!E119:F',
+            }, (err, res) => {
+                if (err) return console.log('The API returned an error: ' + err);
+            const rows = res.data.values;
+            if (rows.length) {
+                rows.map((row) => {
+                    if(String(row[1]).match(/\d+/) == message.member.id){
+                        if(row[0] == 0){
+                            message.channel.send("You have had " + row[0] + " days without missing raids.  Bronze level status is at 14 days.");
+                        }
+                        if(row[0] >= 1 && row[0] <= 13){
+                            message.channel.send("Congratulations!! You have had " + row[0] + " days without missing raids.  Bronze level status is at 14 days.");
+                        }
+                        if(row[0] >= 14 && row[0] <= 29){
+                            message.channel.send("You are at bronze level!! You have had " + row[0] + " days without missing raids.  Silver level status is at 30 days.");
+                        }
+                        if(row[0] >= 30 && row[0] <= 59){
+                            message.channel.send("You are at silver level!! You have had " + row[0] + " days without missing raids.  Gold level status is at 60 days.");
+                        }
+                        if(row[0] >= 60 && row[0] <= 99){
+                            message.channel.send("You are at gold level!! You have had " + row[0] + " days without missing raids.  Platnium level status is at 100 days.");
+                        }
+                        if(row[0] >= 100){
+                            message.channel.send("Congratulations!! You have had " + row[0] + " days without missing raids.  You are platnium status!!!!!");
+                        }
+                    }
+                });
+            }else {
+                console.log('No data found.');
+            }
+            });
+        }       
+    }
+    if(message.content == `${prefix}flairupdate` || message.content == `${prefix}Flairupdate`){
+        if(message.member.id == "406945430967156766"){
+            message.channel.send("Flair is being updated for all guild members")
+            FlairUpdate()
+        } else{
+            message.channel.send(message.member.displayName + ", what do you think you are doing.  Turn back.  I have spoken.")
+        }
+    }
+})
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
