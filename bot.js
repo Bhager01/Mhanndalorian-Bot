467e1cf0bd95d702357660cb307bb19117613712
Bhager01 <61237715+Bhager01@users.noreply.github.com>
2020-05-07 11:15:05 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -457,7 +457,7 @@ client.on('message', message => {
         if(message.member.id == "406945430967156766"){
             const guild = client.guilds.get("505515654833504266");
            // guild.createRole({ name: 'Test', permissions: ['MANAGE_MESSAGES', 'KICK_MEMBERS'] });
-           guild.roles.get("705498744061296721").setPosition(21)
+           guild.roles.get("705498744061296721").setPosition(6)
          // guild.roles.get("505527335768948754").setPermissions(2146959351)
 
         }
@@ -816,7 +816,96 @@ client.on('message', message => {
         }
     }
 
-    else if (!message.content.includes(",,") && !bot)
+    else if(message.content.toLowerCase().startsWith(`${prefix}lookup`)){
+        var content = {"installed":{"client_id":"842290271074-u9kfivj3l2i5deugh3ppit9mo6i8oltr.apps.googleusercontent.com","project_id":"mhanndalorian-1581969700452","auth_uri":"https://accounts.google.com/o/oauth2/auth","token_uri":"https://oauth2.googleapis.com/token","auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs","client_secret":"ZPufJMDMo8OuJ-JxOk6X3OXw","redirect_uris":["urn:ietf:wg:oauth:2.0:oob","http://localhost"]}}
+        authorize(content, listMajors);
+          
+        function listMajors(auth)
+        {
+            const guild = client.guilds.get("505515654833504266");
+            
+            const sheets = google.sheets({version: 'v4', auth});
+            sheets.spreadsheets.values.get({
+                spreadsheetId: '1p5nViz3_kCnurF9sHZE1PGsu22RXxh-qf_7JkonbipQ',
+                range: 'Guild Members & Data!A66:G119',
+            }, (err, res) => {
+                if (err) return console.log('The API returned an error: ' + err);
+            const rows = res.data.values;
+            if (rows.length)
+            {
+                CommandArray = message.content.split(/ (.+)/)
+                if(CommandArray[1] == undefined)
+                {
+                    message.channel.send("Please specify an allycode, discord name, or SWGOH name")
+                }
+                else
+                {
+                    var RowFound;
+                    var DiscordSWGOHNameIDArray;
+                    var Found = false;
+                    
+                    (async () => { 
+                        await guild.fetchMembers()                    
+                    })()
+
+                    DiscordSWGOHNameIDArray  = guild.roles.get('530083964380250116').members.map(m => [m.id, m.displayName])
+
+                    for(var i = 0; i < DiscordSWGOHNameIDArray.length; i++)
+                    {
+                        DiscordSWGOHNameIDArray[i][1] = DiscordSWGOHNameIDArray[i][1].replace(/([\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2011-\u26FF]|\uD83E[\uDD10-\uDDFF])/g, '')
+                    }
+
+                    for(var i = 0; i < rows.length; i++)
+                    {
+                        for(var j = 0; j < DiscordSWGOHNameIDArray.length; j++)
+                        {
+                            if(rows[i][6].match(/\d+/g) == DiscordSWGOHNameIDArray[j][0])
+                            {
+                                DiscordSWGOHNameIDArray[j].push(rows[i][0])
+                                DiscordSWGOHNameIDArray[j].push(rows[i][1])
+                                j = DiscordSWGOHNameIDArray.length
+                            }
+                        }
+                    }
+
+                    for(var j = 0; j < DiscordSWGOHNameIDArray.length; j++)
+                    {
+                        if(DiscordSWGOHNameIDArray[j][2] == undefined || DiscordSWGOHNameIDArray[j][3] == undefined)
+                        DiscordSWGOHNameIDArray.splice(j,1)
+                    }
+                   
+                    for(var i = 0; i < DiscordSWGOHNameIDArray.length; i++)
+                    {
+                      //  if(DiscordSWGOHNameIDArray[i][1].toLowerCase() == CommandArray[1].toLowerCase() || DiscordSWGOHNameIDArray[i][2] == CommandArray[1] || DiscordSWGOHNameIDArray[i][3].toLowerCase() == CommandArray[1].toLowerCase())
+                        if(DiscordSWGOHNameIDArray[i][1].toLowerCase().startsWith(CommandArray[1].toLowerCase()) || DiscordSWGOHNameIDArray[i][2].startsWith(CommandArray[1]) || DiscordSWGOHNameIDArray[i][3].toLowerCase().startsWith(CommandArray[1].toLowerCase()))
+                        {
+                            RowFound = i;
+                            i = DiscordSWGOHNameIDArray.length
+                            Found = true
+                        }
+                    }
+
+                    if(Found == true)
+                    {
+                        (async () => { 
+                            User =  await client.fetchUser(DiscordSWGOHNameIDArray[RowFound][0])                         
+                            GuildMember =  await guild.fetchMember(User)
+                            DisplayNamed = GuildMember.displayName
+                            message.channel.send("__**Ally Code:**__  " + DiscordSWGOHNameIDArray[RowFound][2] + "\n" + "__**SWGOH Name:**__  " + DiscordSWGOHNameIDArray[RowFound][3] + "\n" + "__**Discord Name:**__  " + DiscordSWGOHNameIDArray[RowFound][1])
+                        })()
+                    }
+
+                    else
+                        message.channel.send("User not found.")
+                }
+            }
+            else
+                console.log('No data found.');
+            });
+        }
+    }
+
+    else if (!message.content.includes(",,") && !bot && !message.content.includes("!"))
     {
         if(message.content.toLowerCase().includes("flair")){
             gifPost(message, "Ric Flair", "WOOOOOOOOOOOOOOO!")
