e8eb0773f19b2828dde6626218c75bf995bbfbf6
Bhager01 <61237715+Bhager01@users.noreply.github.com>
2020-11-16 02:17:52 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,10 +1,11 @@
-const { Client, Intents } = require("discord.js");
+const { Intents } = require("discord.js");
+const Discord = require('discord.js');
 const intents = new Intents([
     Intents.NON_PRIVILEGED, // include all non-privileged intents, would be better to specify which ones you actually need
     "GUILD_MEMBERS", // lets you request guild members (i.e. fixes the issue)
     "GUILD_PRESENCES",
 ]);
-const client = new Client({ ws: { intents } });
+const client = new Discord.Client({ ws: { intents } });
 
 const {google} = require('googleapis');
 const fetch = require('node-fetch');
